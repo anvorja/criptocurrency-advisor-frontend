1716523fe2277fb687cf3384126f4ddc62805b0e
anvorja <borja.carlos@correounivalle.edu.co>
2024-05-08 03:12:42 -0500

    fixed deploy

--- src/components/NewCard.jsx
+++ src/components/NewCard.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import {
+  Box,
+  Button,
+  Card,
   CardActionArea,
   CardContent,
-  Typography
+  CardMedia,
+  Typography,
 } from "@mui/material";
 
 function NewCard(mynew) {
